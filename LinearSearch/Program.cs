08df03e69d7e07a6e9a00492de0139039061c426
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-09-10 17:24:52 +0300

    add: linear search

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections;
+
+int[] sequince = new int[6] {1,5,6,-7,3,-4};
+int element = 3;
+
+Console.WriteLine($"index = {LineSearch(sequince, element)}");
+int LineSearch(int[] sequince, int element)
+{
+    for(int i = 0; i < sequince.Length; i++)
+    {
+        if(sequince[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+List<int> listSequince = new List<int>() {1, 5, 6, -7, 3, -4 };
+Console.WriteLine($"index = {LineSearchModified(listSequince, element)}");
+int LineSearchModified(List<int> sequince, int element)
+{
+    sequince.Add(element);
+    int lastIndex = sequince.Count - 1;
+    int i = 0;
+    for (; sequince[i] != element ;)
+    {
+        i++;
+    }
+    sequince.Remove(lastIndex);
+    if (i != lastIndex)
+    {
+        return i;
+    }
+    return -1;
+}
